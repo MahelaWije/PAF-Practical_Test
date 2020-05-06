b9dddcf7e1ab92a22f08cf939dc7d3c759090104
Mahela Wijekoon <mahelawije@gmail.com>
2020-05-06 16:36:37 +0530

    Final Commit

--- PafUserNew/src/model/User.java
+++ PafUserNew/src/model/User.java
@@ -216,7 +216,7 @@ public class User {
 				 * "<td><input name=\"btnUpdate\" type=\"button\"value=\"Update\" class=\"btn btn-secondary\"></td>"
 				 * + "<td><form method=\"post\" action=\"items.jsp\">" +
 				 * "<input name=\"btnRemove\" type=\"submit\" value=\"Remove\"class=\"btn btn-danger\">"
-				 * + "<input name=\"userID\" type=\"hidden\" value=\"" + userID + "\">" +
+				 * + "<input name=\"userID\" type=\"hidden\" value=\"" + userID + "\">"+
 				 * "</form></td></tr>";
 				 */
 			}
